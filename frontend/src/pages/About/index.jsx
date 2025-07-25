9e81c1563cba29b81319f99dfb55b6b0969ee094
asimov <aizhimoug@gmail.com>
2025-07-25 22:17:28 +1200

    update about page

--- frontend/src/pages/About/index.jsx
+++ frontend/src/pages/About/index.jsx
@@ -1,12 +1,51 @@
-import React, { useContext, useEffect } from 'react';
-import {Center, Title} from "@mantine/core";
+import React, {useContext, useEffect, useState} from 'react';
+import {Center, Container, Paper, Text, Title} from "@mantine/core";
+import {API, removeTrailingSlash, showError} from "../../helpers/index.js";
+import {marked} from "marked";
 
 const About = () => {
 
+  const [about, setAbout] = useState('');
+  const [aboutLoaded, setAboutLoaded] = useState(false);
+
+  const displayAbout = async () => {
+    let aboutContent = localStorage.getItem('about');
+    if (aboutContent) {
+      setAbout(aboutContent);
+      return;
+    }
+    const res = await API.get('/api/config/name?name=About');
+    const {code, msg, data} = res.data;
+    if (code === 200) {
+      let content = marked.parse(data);
+      setAbout(content);
+      localStorage.setItem('about', content);
+    } else {
+      showError(msg);
+      setAbout('Loading about content failed...');
+    }
+    setAboutLoaded(true);
+  };
+
+  useEffect(() => {
+    displayAbout().then();
+  }, []);
+
   return (
-      <Center mt={150}>
-        <Title>About Page</Title>
-      </Center>
+      <Container size="lg">
+        {aboutLoaded && about === '' ? (
+            <>
+              <Title order={4}>About</Title>
+              <Text>You can write some contents in setting to show in here. HTML
+                and Markdown are supported.</Text>
+            </>
+        ) :
+        <Container>
+          <div dangerouslySetInnerHTML={{__html: about}}></div>
+        </Container>
+        }
+
+      </Container>
   );
 };
 
