f9c6f9fe0a85fc7978efbfe2fcc4f69af0f49d2a
Asimov <aizhimou@outlook.com>
2025-08-03 22:04:40 +1200

    update api key column length

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE if NOT EXISTS user
     email      VARCHAR(100)                        NULL,
     role       VARCHAR(20)   DEFAULT 'USER'        NOT NULL comment 'ADMIN,USER',
     status     tinyint   DEFAULT 1                 NOT NULL comment '1:enabled; 0:disabled',
-    api_key    VARCHAR(30)                         NULL,
+    api_key    VARCHAR(50)                         NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT user_pk_2 UNIQUE (username),
